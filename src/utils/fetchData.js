6345e1f39d705c1dcb17a91e6e7f69208e31ae8a
PR1NCE-lab <myproacount64@gmail.com>
2023-04-17 19:04:11 +0530

    removing api keys

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,7 +3,7 @@ export const ExerciseOptions ={
     url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': 'c08cfb6b40msh734b8454a5c3c75p15bce1jsnc0f0cc172e01',
+    'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
     
     },
   };
@@ -12,7 +12,7 @@ export const ExerciseOptions ={
     method: 'GET',
     url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
     headers: {
-      'X-RapidAPI-Key': 'c08cfb6b40msh734b8454a5c3c75p15bce1jsnc0f0cc172e01',
+      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
